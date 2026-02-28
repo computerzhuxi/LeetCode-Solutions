c6db52ab5771754828e41cdaaf01ab55e3056159
computerzhuxi <computerzhuxi@gmail.com>
2026-02-28 14:35:52 +0800

    添加 1680. 连接连续二进制数字 题解

---
+++ 1680-连接连续二进制数字/README.md
@@ -0,0 +1,14 @@
+# 1680. 连接连续二进制数字
+
+## 题目描述
+
+[1680-连接连续二进制数字](https://leetcode.cn/problems/concatenation-of-consecutive-binary-numbers/)
+
+## 思路
+
+直接暴力枚举，注意每次枚举时都进行一次mod操作
+
+## 复杂度
+
+- 时间复杂度：O(n)
+- 空间复杂度：O(1)
---
+++ 1680-连接连续二进制数字/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        constexpr int MOD = 1'000'000'007;
+        long long ans = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            int w = bit_width((uint32_t) i);    // 计算i的二进制的宽度
+            ans = (ans << w | i) % MOD;
+        } 
+        return ans;
+    }
+};
