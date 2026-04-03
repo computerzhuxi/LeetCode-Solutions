c691c4af02c366e5ad91a2f7fd36a90191ee0472
computerzhuxi <computerzhuxi@gmail.com>
2026-04-03 09:51:46 +0800

    添加 24. 两两交换链表中的节点 题解

---
+++ 24-两两交换链表中的节点/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* temp = dummyHead;
+        while (temp->next != nullptr && temp->next->next != nullptr) {
+            ListNode* node1 = temp->next;
+            ListNode* node2 = temp->next->next;
+            temp->next = node2;
+            node1->next = node2->next;
+            node2->next = node1;
+            temp = node1;
+        }
+        ListNode* ans = dummyHead->next;
+        delete dummyHead;
+        return ans;
+    }
+};
