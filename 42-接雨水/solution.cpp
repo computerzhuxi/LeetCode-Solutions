57e3b4b495da6f3c43b97c0ff39377e87070d484
computerzhuxi <computerzhuxi@gmail.com>
2026-05-22 21:03:04 +0800

    添加 46. 全排列 题解

---
+++ 42-接雨水/solution.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end())
+            {
+                return {i, it->second};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
